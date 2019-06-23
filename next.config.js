c229dd37adf32dcdab9e10b3e072416719956a88
Robert Koch <robert@kochie.io>
2019-06-24 02:21:01 +0800

    modified pwa and aliases

--- next.config.js
+++ next.config.js
@@ -8,7 +8,27 @@ const plugins = [withMDX, withTypescript, withOffline, withLess]
 
 const config = {
     cssModules: true,
-    target: 'serverless'
+    target: 'serverless',
+    workboxOpts: {
+    swDest: 'static/service-worker.js',
+    runtimeCaching: [
+      {
+        urlPattern: /^https?.*/,
+        handler: 'NetworkFirst',
+        options: {
+          cacheName: 'https-calls',
+          networkTimeoutSeconds: 15,
+          expiration: {
+            maxEntries: 150,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+          },
+          cacheableResponse: {
+            statuses: [0, 200],
+          },
+        },
+      },
+    ],
+  },
 }
 
 module.exports = withPlugins(plugins, config)
