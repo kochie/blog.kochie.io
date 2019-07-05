718feeb79c9e7510f863a1c37ccf034d9a0e922c
Robert Koch <robert@kochie.io>
2019-07-06 05:35:14 +0800

    updated atricle less

--- styles/article.less.d.ts
+++ styles/article.less.d.ts
@@ -1,12 +1,13 @@
 declare const styles: {
-  readonly card: string
-  readonly container: string
-  readonly title: string
-  readonly glass: string
-  readonly text: string
-  readonly subText: string
-  readonly heading: string
-  readonly details: string
-  readonly imageForeground: string
-}
-export = styles
+  readonly "card": string;
+  readonly "container": string;
+  readonly "title": string;
+  readonly "glass": string;
+  readonly "text": string;
+  readonly "subText": string;
+  readonly "heading": string;
+  readonly "details": string;
+  readonly "imageForeground": string;
+};
+export = styles;
+
