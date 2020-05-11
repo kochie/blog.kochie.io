07f9dbaa2c0d9720a75f6e0b3122b83288e2cc34
Robert Koch <robekoc@amazon.com>
2020-05-12 02:20:40 +1000

    fixed site build

---
+++ postcss.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    "plugins": [
+      "postcss-flexbugs-fixes",
+      [
+        "postcss-preset-env",
+        {
+          "autoprefixer": {
+            "flexbox": "no-2009"
+          },
+          "stage": 1,
+        //   "features": {
+        //     "custom-properties": false
+        //   }
+        }
+      ]
+    ]
+  }
+  
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import React, { ReactElement } from 'react'
-import App from 'next/app'
+import React, { ReactElement, useEffect } from 'react'
+import { AppProps } from 'next/app'
 import Router from 'next/router'
 // import ReactGA from 'react-ga'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
 import { DefaultSeo } from 'next-seo'
+// import { useRouter } from 'next/router'
 
 import '../styles/main.css'
 
@@ -15,20 +16,29 @@ config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatica
 Router.events.on('routeChangeComplete', (url) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
   // @ts-ignore
-  fathom('trackPageview')
+  fathom.trackPageview({ url })
 })
 
-export default class MyApp extends App {
-  render(): ReactElement {
-    const { Component, pageProps } = this.props
-    return (
-      <>
-        <DefaultSeo {...SEO} />
-        <Component {...pageProps} />
-      </>
-    )
-  }
+function App({ Component, pageProps }: AppProps): ReactElement {
+  useEffect(() => {
+    const tracker = window.document.createElement('script')
+    const firstScript = window.document.getElementsByTagName('script')[0]
+    tracker.defer = true
+    tracker.setAttribute('site', 'QFZGKZMZ')
+    tracker.setAttribute('spa', 'auto')
+    tracker.src = 'https://cdn.usefathom.com/script.js'
+    firstScript.parentNode?.insertBefore(tracker, firstScript)
+  }, [])
+
+  return (
+    <>
+      <DefaultSeo {...SEO} />
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default App
 
 // Only uncomment this method if you have blocking data requirements for
 // every single page in your application. This disables the ability to
