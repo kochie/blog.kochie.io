539bef6b8cc270ea0a9e9d527b3b6e0b6b9511de
Robert Koch <robert@kochie.io>
2020-05-12 12:05:54 +1000

    fixed postcss custom media queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "plugins": [
-      "postcss-flexbugs-fixes",
-      [
-        "postcss-preset-env",
-        {
-          "autoprefixer": {
-            "flexbox": "no-2009"
-          },
-          "stage": 1,
-        //   "features": {
-        //     "custom-properties": false
-        //   }
-        }
-      ]
-    ]
+  "plugins": {
+    "postcss-flexbugs-fixes": {},
+    "postcss-preset-env": {
+      importFrom: [{
+        "customMedia": {
+          '--small-viewport': '(min-width: 500px)',
+          '--medium-viewport': '(min-width: 600px)',
+          '--large-viewport': '(min-width: 1100px)',
+        }}
+      ],
+      "autoprefixer": {
+        "flexbox": "no-2009"
+      },
+      "stage": 1
+    }
   }
-  
+}
