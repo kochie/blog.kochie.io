17bbb1b2eb9165ac8e728abc2f1e94df904e2f6f
Robert Koch <robekoc@amazon.com>
2020-05-14 00:21:58 +1000

    updated sw path

--- next.config.js
+++ next.config.js
@@ -10,8 +10,8 @@ const config = {
   transformManifest: (manifest) => ['/'].concat(manifest), // add the homepage to the cache
   // Trying to set NODE_ENV=production when running yarn dev causes a build-time error so we
   // turn on the SW in dev mode so that we can actually test it
-  generateInDevMode: false,
-  handleImages: ['jpeg', 'jpg', 'png', 'svg', 'webp', 'gif'],
+  generateInDevMode: true,
+  // handleImages: ['jpeg', 'jpg', 'png', 'svg', 'webp', 'gif'],
   workboxOpts: {
     maximumFileSizeToCacheInBytes: 1024 * 1024 * 10,
     swDest: 'service-worker.js',
