ed2c63730f528697fb74352832fffb324faf609a
Robert Koch <robert@kochie.io>
2020-11-11 12:49:21 +1100

    designed new article metadata parser

---
+++ src/lib/article-metadata.ts
@@ -0,0 +1,33 @@
+import { Article } from 'articles.json'
+import { opendir, readFile } from 'fs/promises'
+import { join } from 'path'
+import { readingTime } from 'reading-time-estimator'
+import { validate, ValidationError } from 'jsonschema'
+import TOML from '@iarna/toml'
+
+const collectArticles = async (): Promise<Article[]> => {
+  const articles: Article[] = []
+
+  const dir = await opendir(join(__dirname, '../articles'))
+  for await (const dirent of dir) {
+    // console.log(dirent.name);
+    if (dirent.isDirectory()) {
+      try {
+        const article = await readArticleToml(join(dirent.name, 'article.toml'))
+        articles.push(article)
+      } catch (err) {}
+    }
+  }
+
+  return articles
+}
+
+const readArticleToml = async (filename: string): Promise<Article> => {
+  const file = await readFile(filename)
+  const article_schema = {}
+  const article = TOML.parse(file.toString()) as unknown
+  validate(article, article_schema, { throwError: true })
+  return article as Article
+}
+
+export { collectArticles }
