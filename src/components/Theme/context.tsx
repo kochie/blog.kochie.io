e377fb743c49489e3be6e75c32801d04152cf791
Robert Koch <robert@kochie.io>
2020-11-13 13:24:36 +1100

    added ref to theme context

--- src/components/Theme/context.tsx
+++ src/components/Theme/context.tsx
@@ -1,10 +1,12 @@
 import React, {
+  MutableRefObject,
   PropsWithChildren,
   ReactElement,
   createContext,
   useCallback,
   useContext,
   useEffect,
+  useRef,
   useState,
 } from 'react'
 
@@ -26,28 +28,41 @@ const ThemeContext = createContext<ThemeStateContext>({
 
 // let i = 0
 
+function _useTheme(
+  initialTheme: THEME = THEME.system
+): [MutableRefObject<THEME>, (theme: THEME) => void] {
+  const [theme, _setTheme] = useState<THEME>(initialTheme)
+  const savedTheme = useRef<THEME>(theme)
+
+  const setTheme = (theme: THEME): void => {
+    savedTheme.current = theme
+    _setTheme(theme)
+  }
+
+  return [savedTheme, setTheme]
+}
+
 const ThemeProvider = ({
   children,
 }: PropsWithChildren<unknown>): ReactElement => {
-  const [theme, setTheme] = useState<THEME>(THEME.system)
-  const value = { theme, setTheme }
-  console.log('RENDER', theme)
+  const [theme, setTheme] = _useTheme(THEME.system)
+  const value = { theme: theme.current, setTheme }
+
+  const switchToLight = useCallback((e: MediaQueryListEvent): void => {
+    if (e.matches && theme.current === THEME.system) {
+      console.log(theme)
+      document.body.classList.remove('dark-theme')
+    }
+  }, [theme])
+
+  const switchToDark = useCallback((e: MediaQueryListEvent): void => {
+    if (e.matches && theme.current === THEME.system) {
+      console.log(theme)
+      document.body.classList.add('dark-theme')
+    }
+  }, [theme])
 
   useEffect(() => {
-    const switchToLight = (e: MediaQueryListEvent): void => {
-      if (e.matches && theme === THEME.system) {
-        console.log(theme)
-        document.body.classList.remove('dark-theme')
-      }
-    }
-
-    const switchToDark = (e: MediaQueryListEvent): void => {
-      if (e.matches && theme === THEME.system) {
-        console.log(theme)
-        document.body.classList.add('dark-theme')
-      }
-    }
-    console.log('USE EFFECT')
     window
       .matchMedia('(prefers-color-scheme: light)')
       .addEventListener('change', switchToLight)
@@ -64,16 +79,15 @@ const ThemeProvider = ({
         .matchMedia('(prefers-color-scheme: dark)')
         .removeEventListener('change', switchToDark)
     }
-  }, [theme])
+  }, [switchToDark, switchToLight])
 
   useEffect(() => {
     const theme = window.localStorage.getItem('theme')
-    console.log('GOT THEME', theme)
     if (theme) setTheme(theme as THEME)
-  }, [])
+  })
 
   useEffect(() => {
-    switch (theme) {
+    switch (theme.current) {
       case THEME.dark: {
         document.body.classList.add('dark-theme')
         break
@@ -93,7 +107,7 @@ const ThemeProvider = ({
       }
     }
 
-    window.localStorage.setItem('theme', theme)
+    window.localStorage.setItem('theme', theme.current)
   }, [theme])
 
   return <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
