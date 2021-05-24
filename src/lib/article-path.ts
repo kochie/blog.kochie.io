09c6bb44b444e0ec9ea983e838ae33fdf9cc8cf8
Robert Koch <robert@kochie.io>
2021-05-24 11:46:01 +1000

    Fixed ESLint errors

--- src/lib/article-path.ts
+++ src/lib/article-path.ts
@@ -94,7 +94,7 @@ export interface ArticleMetadata {
   indexPath: string
   articleDir: string
   publishedDate: string
-  editedDate: string
+  editedDate?: string
   title: string
   blurb: string
 }
