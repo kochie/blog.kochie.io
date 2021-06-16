0b0cf5f40207b9001ead2a8fde3f847b075a91d4
Robert Koch <robert@kochie.io>
2021-06-16 17:39:08 +1000

    adds rehype plugin for lqip

--- src/components/Article/__tests__/Article.test.tsx
+++ src/components/Article/__tests__/Article.test.tsx
@@ -13,6 +13,7 @@ const testArticle: ArticleMetadata = {
   jumbotron: {
     url: '/test.png',
     alt: 'alt text',
+    lqip: "AAAAAAAAAAAA"
   },
   articleDir: 'articleDir',
   readTime: '1 min read',
