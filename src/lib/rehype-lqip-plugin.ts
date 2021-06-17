278bd27dbf8a0699db6fd72bb8f9a908f693f504
Robert Koch <robert@kochie.io>
2021-06-17 15:18:32 +1000

    reworked image compression

--- src/lib/rehype-lqip-plugin.ts
+++ src/lib/rehype-lqip-plugin.ts
@@ -1,5 +1,4 @@
 import visit from 'unist-util-visit'
-// import { Node } from 'unist'
 // @ts-expect-error: No imports defined
 import is from 'hast-util-is-element'
 import { join } from 'path'
@@ -9,25 +8,19 @@ export default function (): (tree: any) => Promise<void> {
   return transformer
 
   async function transformer(tree: any): Promise<void> {
-    // console.log('IS IT FIRING>>>>>>>.')
     const nodes: any[] = []
     visit(tree, 'element', (node) => {
       if (is(node, 'img')) {
-        // console.log('HELLO', node)
         nodes.push(node)
       }
-      // Do stuff with heading nodes
     })
 
     await Promise.all(
       nodes.map(async (node: any) => {
-        // console.log(node)
-        node.properties.lqip = lqip(
+        node.properties.lqip = await lqip(
           join(process.env.PWD || '', 'public', node.properties.src)
         )
       })
     )
-
-    // console.log(nodes)
   }
 }
