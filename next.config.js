fd167fa39d3aa5d90f0d53287099021ef67d3ba8
Robert Koch <robert@kochie.io>
2021-06-18 11:12:01 +1000

    removed plugins temporarily

--- next.config.js
+++ next.config.js
@@ -1,26 +1,25 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const withPlugins = require('next-compose-plugins')
-const withOffline = require('next-offline')
-const { withSentryConfig } = require('@sentry/nextjs')
+// const withOffline = require('next-offline')
+// const { withSentryConfig } = require('@sentry/nextjs')
 
-const plugins = [withOffline, withSentryConfig]
+const plugins = []
+// const plugins = []
 
 const config = {
   target: 'serverless',
-  webpack5: true,
   webpack(config) {
     config.module.rules.push({
       test: /\.ya?ml$/,
       type: 'json',
       use: 'yaml-loader',
     })
-    config.externals.sharp = "commonjs sharp"
+    config.externals = {
+      ...config.externals, 
+      sharp: 'sharp',
+    }
 
     return config
-  },
-  i18n: {
-    locales: ['en-AU'],
-    defaultLocale: 'en-AU',
   },
   workboxOpts: {
     swDest: process.env.NEXT_EXPORT
