d15dc800d877d7c09c5d2824ebcc7895fcbbb265
Robert Koch <robert@kochie.io>
2021-06-18 13:52:11 +1000

    added secret for dependabot

--- src/lib/shrink.ts
+++ src/lib/shrink.ts
@@ -3,5 +3,7 @@ import jimp from 'jimp'
 export async function lqip(filename: string): Promise<string> {
   const image = await jimp.read(filename)
   image.resize(jimp.AUTO, 10)
-  return await image.getBase64Async(jimp.MIME_PNG)
+  const base64 = await image.getBase64Async(jimp.MIME_PNG)
+  console.log(base64)
+  return base64
 }
