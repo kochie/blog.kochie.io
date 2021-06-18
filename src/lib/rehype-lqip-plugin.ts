0191a9794abdcbe428eba2abb7cdc498ad883e91
Robert Koch <robert@kochie.io>
2021-06-18 20:14:43 +1000

    added better types for rehype plugin

--- src/lib/rehype-lqip-plugin.ts
+++ src/lib/rehype-lqip-plugin.ts
@@ -1,25 +1,30 @@
-import visit from 'unist-util-visit'
-// @ts-expect-error: No imports defined
-import is from 'hast-util-is-element'
+import { Node, visit } from 'unist-util-visit'
+import { Element, isElement } from 'hast-util-is-element'
 import { join } from 'path'
 import { lqip } from './shrink'
 
-export default function (): (tree: any) => Promise<void> {
+export default function (): (tree: Node) => Promise<void> {
   return transformer
 
-  async function transformer(tree: any): Promise<void> {
-    const nodes: any[] = []
+  async function transformer(tree: Node): Promise<void> {
+    const nodes: Element[] = []
     visit(tree, 'element', (node) => {
-      if (is(node, 'img')) {
+      if (isElement(node, 'img')) {
         nodes.push(node)
       }
     })
 
     await Promise.all(
-      nodes.map(async (node: any) => {
-        node.properties.lqip = await lqip(
-          join(process.env.PWD || '', 'public', node.properties.src)
-        )
+      nodes.map(async (node: Element) => {
+        if (node?.properties?.src) {
+          node.properties.lqip = await lqip(
+            join(
+              process.env.PWD || '',
+              'public',
+              node.properties.src.toString()
+            )
+          )
+        }
       })
     )
   }
