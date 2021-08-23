da7bbac24b824bb439e35144e5a75c6944082d4c
Robert Koch <robert@kochie.io>
2021-08-23 14:08:24 +1000

    added static build of rss feeds

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -19,6 +19,14 @@ const Page = ({ children }: { children: ReactNode }): ReactElement => {
             src: 'https://linkedin.com/in/rkkochie',
             goal: 'RMXXVNIC',
           },
+          {
+            name: 'rss',
+            src: `https://${
+              process.env.NEXT_PUBLIC_PROD_URL ||
+              process.env.NEXT_PUBLIC_VERCEL_URL
+            }/feed/rss.xml`,
+            goal: 'PZQY507K',
+          },
         ]}
       />
     </div>
