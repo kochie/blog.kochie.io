e9f72920785db9914b7ce165adcf8a0646dda88f
Robert Koch <robert@kochie.io>
2022-10-17 12:49:55 +1300

    updtes tests, adds Revue component (#630)
    * updtes tests, adds Revue component
    
    * adds icon for revue

---
+++ src/components/Revue/__tests__/Revue.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { ReactTestRenderer, act, create } from 'react-test-renderer'
+
+import Revue from '..'
+
+describe('Quote Component', () => {
+  test('renders correctly', () => {
+    let tree: ReactTestRenderer
+
+    act(() => {
+      tree = create(<Revue />)
+    })
+
+    // @ts-expect-error tree will be assigned
+    expect(tree.toJSON()).toMatchSnapshot()
+  })
+})
