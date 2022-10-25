73160654d687fbedef7b27f607ff76fa15109b82
Robert Koch <robert@kochie.io>
2022-10-25 13:34:04 +1100

    (fix): tweaking some issues (#649)
    * fixed topbar links
    
    * changed image container to relative
    
    * tweak revue component

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -6,14 +6,20 @@ import style from './Topbar.module.css'
 const TopBar = (): ReactElement => {
   return (
     <div className="flex flex-row justify-center items-center list-none bg-gray-500 m-0 p-0 fixed w-screen z-40">
-      <Link href={'/authors'} passHref>
-        <div className={`${style.child} ${style.heading}`}>Authors</div>
+      <Link href="/authors">
+        <a>
+          <div className={`${style.child} ${style.heading}`}>Authors</div>
+        </a>
       </Link>
-      <Link href={'/'} passHref>
-        <div className={`${style.child} ${style.heading}`}>Articles</div>
+      <Link href="/">
+        <a>
+          <div className={`${style.child} ${style.heading}`}>Articles</div>
+        </a>
       </Link>
-      <Link href={'/tags'} passHref>
-        <div className={`${style.child} ${style.heading}`}>Tags</div>
+      <Link href="/tags">
+        <a>
+          <div className={`${style.child} ${style.heading}`}>Tags</div>
+        </a>
       </Link>
     </div>
   )
