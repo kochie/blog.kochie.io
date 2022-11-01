ed8cde94fa1d669a00482c884cdd89f7cd4455dd
Robert Koch <robert@kochie.io>
2022-11-01 19:40:45 +1100

    added og api

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -61,6 +61,7 @@ export default async function handler(req: NextRequest) {
     {
       width: 1200,
       height: 630,
+      debug: true,
       fonts: [
         {
           name: 'Roboto Condensed',
