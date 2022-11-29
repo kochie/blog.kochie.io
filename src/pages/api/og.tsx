f8cbcba33a892441858235ac94e0f5cbb0b8b94c
Robert Koch <robert@kochie.io>
2022-11-29 14:09:05 +1100

    tweak og creation

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -77,15 +77,13 @@ export default async function handler(req: NextRequest) {
             </span>
           </div>
         </div>
-        <div tw="absolute bottom-0 right-0 m-8 z-50">
-          <img
-            alt=""
-            src={`https://${process.env.VERCEL_URL}/images/authors/${author}.png`}
-            width="100"
-            height="100"
-            tw="rounded-3xl"
-          />
-        </div>
+        <img
+          alt=""
+          src={`https://${process.env.VERCEL_URL}/images/authors/${author}.png`}
+          width="100"
+          height="100"
+          tw="rounded-3xl absolute bottom-0 right-0 m-8 z-50"
+        />
       </div>
     ),
     { width: 1200, height: 630, debug: true }
