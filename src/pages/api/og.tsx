d1fd1a90f594ea61fa697241dfee004c9de7c56b
Robert Koch <robert@kochie.io>
2022-11-29 13:30:05 +1100

    tweak og creation

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -60,7 +60,27 @@ export default async function handler(req: NextRequest) {
     (
       <div
         style={{
+          width: '100%',
+          height: '100%',
           display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          fontSize: 128,
+          background: 'lavender',
+        }}
+      >
+        Hello!
+      </div>
+    ),
+    { width: 1200, height: 630, debug: true }
+  )
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
           height: '100vh',
           width: '100vw',
         }}
