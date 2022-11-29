8749588781c0b31edcb6f7ab288b6baacdace21e
Robert Koch <robert@kochie.io>
2022-11-30 01:21:45 +1100

    smaller og image

--- src/lib/next-seo.config.ts
+++ src/lib/next-seo.config.ts
@@ -13,6 +13,11 @@ export const NEXT_SEO_DEFAULT: NextSeoProps = {
     title: 'Kochie Engineering',
     description:
       'My blog about software engineering, programming, and technology. I write about stuff I see around the internet.',
+    images: [
+      {
+        url: `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/images/og.jpg`,
+      },
+    ],
   },
   twitter: {
     handle: '@kochie',
