ba342855626f9eea6b4b8e90aa0bdf19bfaec89b
Robert Koch <robert@kochie.io>
2023-03-09 19:46:38 +1100

    encode uri for image

--- src/app/articles/[articleId]/page.tsx
+++ src/app/articles/[articleId]/page.tsx
@@ -33,13 +33,15 @@ export async function generateMetadata({
   const articleId = params.articleId
   const articleMetadata = await getArticleMetadata(articleId)
 
-  const imageUrl = `https://${
-    process.env.NEXT_PUBLIC_PROD_URL || process.env.NEXT_PUBLIC_VERCEL_URL
-  }/api/og?title=${encodeURIComponent(
-    articleMetadata.title
-  )}&author=${encodeURIComponent(
-    articleMetadata.author
-  )}&imageUrl=${encodeURIComponent(articleMetadata.jumbotron.url)}`
+  const imageUrl = encodeURI(
+    `https://${
+      process.env.NEXT_PUBLIC_PROD_URL || process.env.NEXT_PUBLIC_VERCEL_URL
+    }/api/og?title=${encodeURIComponent(
+      articleMetadata.title
+    )}&author=${encodeURIComponent(
+      articleMetadata.author
+    )}&imageUrl=${encodeURIComponent(articleMetadata.jumbotron.url)}`
+  )
 
   return {
     title: articleMetadata.title,
