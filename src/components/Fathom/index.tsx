4c19a1fef65ba1d5508d974410db713e864cde54
Robert Koch <robert@kochie.io>
2023-03-10 09:49:00 +1100

    fixes fathom path

--- src/components/Fathom/index.tsx
+++ src/components/Fathom/index.tsx
@@ -5,30 +5,37 @@ import { useEffect } from 'react'
 // Broken until this is fixed.
 // https://github.com/vercel/next.js/issues/42800
 
-import { usePathname } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 
 export default function Fathom() {
   const pathname = usePathname()
-  // const searchParams = useSearchParams()
+  const searchParams = useSearchParams()
 
-  // console.log(pathname, searchParams.toString())
+  console.log(pathname, searchParams.toString())
+
+  // Initialize Fathom when the app loads
+  // Example: yourdomain.com
+  //  - Do not include https://
+  //  - This must be an exact match of your domain.
+  //  - If you're using www. for your domain, make sure you include that here.
+  load('QFZGKZMZ', {
+    includedDomains: ['blog.kochie.io'],
+    url: 'https://kite.kochie.io/script.js',
+    spa: 'auto',
+  })
 
   useEffect(() => {
-    load('QFZGKZMZ', {
-      includedDomains: ['blog.kochie.io'],
-      url: 'https://kite.kochie.io/script.js',
-      spa: 'auto',
+    const url = new URL(pathname, 'https://blog.kochie.io')
+    searchParams.forEach((value, key) => {
+      url.searchParams.append(key, value)
     })
 
-    // Initialize Fathom when the app loads
-    // Example: yourdomain.com
-    //  - Do not include https://
-    //  - This must be an exact match of your domain.
-    //  - If you're using www. for your domain, make sure you include that here.
-    trackPageview()
+    trackPageview({
+      url: url.toString(),
+    })
 
     // Record a pageview when route changes
-  }, [pathname])
+  }, [pathname, searchParams])
 
   return null
 }
