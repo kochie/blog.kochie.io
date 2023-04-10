d319e274e0d8271395399900f48170273c59c783
Robert Koch <robert@kochie.io>
2023-04-10 12:04:22 +1000

    added profiling

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,16 +2,24 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs'
+import { ProfilingIntegration } from '@sentry/profiling-node'
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://93d1763e43d24b2885baed0a99a74b02@o157203.ingest.sentry.io/5779239',
+  dsn:
+    SENTRY_DSN ||
+    'https://93d1763e43d24b2885baed0a99a74b02@o157203.ingest.sentry.io/5779239',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
+  profilesSampleRate: 1.0, // Profiling sample rate is relative to tracesSampleRate
+  integrations: [
+    // Add profiling integration to list of integrations
+    new ProfilingIntegration(),
+  ],
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-});
+})
