d7e78a3fd005c1682f175bc9111f4f6a9d669003
Robert Koch <robert@kochie.io>
2023-04-10 12:06:26 +1000

    added replays

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -2,16 +2,26 @@
 // The config you add here will be used whenever a page is visited.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from '@sentry/nextjs'
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://93d1763e43d24b2885baed0a99a74b02@o157203.ingest.sentry.io/5779239',
+  dsn:
+    SENTRY_DSN ||
+    'https://93d1763e43d24b2885baed0a99a74b02@o157203.ingest.sentry.io/5779239',
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
-});
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  integrations: [new Sentry.Replay()],
+})
