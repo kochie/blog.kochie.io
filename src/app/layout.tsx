6314417dd7c6729698a021d8bea25f9654102029
Robert Koch <robert@kochie.io>
2023-05-04 23:07:27 +1000

    head tag added to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -90,6 +90,7 @@ export async function generateMetadata(): Promise<Metadata> {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
+      <head />
       <body>
         <Suspense fallback={null}>
           <Fathom />
